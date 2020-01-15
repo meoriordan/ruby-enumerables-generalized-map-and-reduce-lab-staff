7fa4b7f1070d9099d6ee4b51952ed0f998a6adf6
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-01-15 03:07:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map (source_array)
+  new = []
+  i = 0 
+  while i < source_array.length do 
+    new.push(yield(source_array[i]))
+    i+=1 
+  end 
+  return new 
+end
+
+def reduce(source_array, sv=nil)
+	if sv
+		sum=sv
+		i = 0
+	else
+		sum = source_array[0]
+		i = 1
+	while i < source_array.length do
+		yield(sum, source_array[i])
+	end
+	sum
+end
